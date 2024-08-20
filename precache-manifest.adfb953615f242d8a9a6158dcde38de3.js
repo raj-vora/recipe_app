6fbdf2dd3b82b77f6ac085eae866d0f465689985
rajvora <rajvora1998@gmail.com>
2024-08-20 10:55:54 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.adfb953615f242d8a9a6158dcde38de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f96f572227e8f7b5a117f518446d5c8",
+    "url": "/recipe_app/index.html"
+  },
+  {
+    "revision": "169fa61ccf68411402d3",
+    "url": "/recipe_app/static/css/main.5f05f5fa.chunk.css"
+  },
+  {
+    "revision": "cb678517df29df23999a",
+    "url": "/recipe_app/static/js/2.b6f95cbc.chunk.js"
+  },
+  {
+    "revision": "20da1371f36bdba5e36ead91be246c0d",
+    "url": "/recipe_app/static/js/2.b6f95cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169fa61ccf68411402d3",
+    "url": "/recipe_app/static/js/main.ea1ce474.chunk.js"
+  },
+  {
+    "revision": "d88f8bd62fab1ec47ba9",
+    "url": "/recipe_app/static/js/runtime-main.a59400d6.js"
+  }
+]);
